f8c7f407e51353381a50420ba9eb1303380d1488
Mauricio Matus Furniel <mauriciomatusfurniel@Mauricios-iMac.local>
2023-01-22 15:45:43 -0300

    Commit02:agregamos un .readme para explicar que hcae el script y sus razones

---
+++ README.md
@@ -0,0 +1,12 @@
+Ambos scripts en este directorio crean un bot que prueba la ejecución
+de todos lo programas mencionados y borra archivos de carpetas que debieran
+estar vacias para que sistemas determinados funcionen mas rapido. Los hice en 
+un esfuerzo de automatizar la mantención de Imac del 2015 de los cualens tengo que mantener
+en laboratorio. Son aproximadamente 70 equipos por lo que hacerlo manualemente es perder
+tiempo, y es una acción que perfectamente se puede replicar¡/automatizar.
+
+No usa ningun modulo externo, solo es un bot hecho con un script BASH que 
+ejecuta programas, espera, los apaga y también borra la info de ciertos directorios
+que los alumnos tienden a llenar.
+
+Sumare cambiar elementos de manera automatica como el fonde de pantalla.
--- probador_programas_b.sh
+++ probador_programas_b.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# Este es un script/bot para automatizar prueba de ejecuciópn de programas.
+# Este es un script/bot para automatizar prueba de ejecuciópn de programas y borrado de archivos.
 # la tarea semanal de mantener un laboratorio de computadores implica ver si softwares determinados funcionan.
 # Script funciona con modulos nativos de Bash. No es necesario instalar nada extra.
+# hay que darle full acceso al disco al terminal para borrar archivos
+# 22/Enero/2023 agregué el borrado de archivos de algunas carpetas que ralentizan sistemas.
+# por lo que se convierte en bot de mantención de sistema completo.
 
 
-
-#!/bin/bash
+echo "Borramos archivos de papelera"
+rm -rf ~/.Trash/*
+#rm -rf ~/.Downloads/*
+#rm -rf ~/.Library/Caches/*
+#rm -rf ~/.Documents/*
 
 echo "Ejecutamos programa en secuencia PS, AE y PR"
 (open -a "Adobe Photoshop CC 2018") &
