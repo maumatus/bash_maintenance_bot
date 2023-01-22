a68d7a2600428f957fb495161bec5a32bfe466b6
Mauricio Matus Furniel <mauriciomatusfurniel@Mauricios-iMac.local>
2023-01-22 16:50:31 -0300

    Commit03:modificamos la estructura con lógica condicional. Posteriormente agregaremos comprobación revisando con boolean si el proceso inicio ok y termino ok, hasta ahora ejecuta sin problemas todos los software excepto premiere. Agregar tambien la suite de office, ProTools y resto de adobe

--- probador_programas_b.sh
+++ probador_programas_b.sh
@@ -1,29 +1,75 @@
 #!/bin/bash
 
-# Este es un script/bot para automatizar prueba de ejecuciópn de programas y borrado de archivos.
-# la tarea semanal de mantener un laboratorio de computadores implica ver si softwares determinados funcionan.
+# Este es un script/bot para automatizar prueba de ejecuciópn de programas y borrado de archivos de algunos directorios específicos.
 # Script funciona con modulos nativos de Bash. No es necesario instalar nada extra.
 # hay que darle full acceso al disco al terminal para borrar archivos
 # 22/Enero/2023 agregué el borrado de archivos de algunas carpetas que ralentizan sistemas.
-# por lo que se convierte en bot de mantención de sistema completo.
 
 
-echo "Borramos archivos de papelera"
-rm -rf ~/.Trash/*
-#rm -rf ~/.Downloads/*
-#rm -rf ~/.Library/Caches/*
-#rm -rf ~/.Documents/*
+echo "Borramos archivos de papelera y descargas (trash and downloads)"
+if rm -rf ~/.Trash/*; then
+    echo "borrado de papelera ok"
+else
+    echo "no hay archivos para borrar"
+fi
 
-echo "Ejecutamos programa en secuencia PS, AE y PR"
-(open -a "Adobe Photoshop CC 2018") &
-sleep 5
-(open -a "Adobe After Effects CC 2018") &
-sleep 5
-(open -a "Adobe Premiere Pro CC 2019" || "Adobe Premiere Pro CC 2020" || "Adobe Premiere Pro CC 2021" || "Adobe Premiere Pro CC 2022") &
-#echo $!
-echo "suspendemos ejecución por 5 minutos"
-sleep 60
-echo "matamos todos los procesos"
-pkill -f "Adobe Photoshop CC 2018"
-pkill -f "Adobe After Effects CC 2018"
-pkill -f "Adobe Premiere Pro CC 2019" || "Adobe Premiere Pro CC 2020" || "Adobe Premiere Pro CC 2021" || "Adobe Premiere Pro CC 2022"
+#if rm -rf ~/.Downloads/*; then
+    #echo "borrado de descargas ok"
+#else
+    #echo "no hay archivos en carpeta descargas"
+#fi
+sleep 2
+
+echo "Ahora ejecutamos programa en secuencia: Photoshop, After Effects y Premiere"
+if (open -a "Adobe Photoshop CC 2018"); then
+    echo "Photoshop ok"
+else
+    echo "problemas con photoshop, revisar manualmente"
+fi
+sleep 2
+
+if (open -a "Adobe After Effects CC 2018"); then
+    echo "After Effects ok"
+else
+    echo "problemas con photoshop, revisar manualmente"
+fi
+sleep 2
+
+if (open -a "Adobe Premiere Pro CC 2019" || "Adobe Premiere Pro CC 2020" || "Adobe Premiere Pro CC 2021" || "Adobe Premiere Pro CC 2022"); then
+    echo "Premiere ok"
+else
+    echo "problemas con premiere, revisar manualmente"
+fi
+sleep 2
+
+echo "todos los programas iniciaron correcto, suspendemos ejecución bot por 8 minuto para que terminen de cargar"
+sleep 480
+
+echo "reiniciamos bot y este cerrara todos los programas que prendio"
+sleep 2
+
+if pkill -f "Adobe Photoshop CC 2018"; then
+    echo "Photoshop terminado"
+else
+    echo "problemas para terminar photoshop, revisar manualmente"
+fi
+
+sleep 2
+
+if pkill -f "Adobe After Effects CC 2018"; then
+    echo "After Effects terminado"
+else
+    echo "problemas para terminar After Effects, revisar manualmente"
+fi
+
+sleep 2
+
+if pkill -f "Adobe Premiere Pro CC 2019" || "Adobe Premiere Pro CC 2020" || "Adobe Premiere Pro CC 2021" || "Adobe Premiere Pro CC 2022"; then
+    echo "Premiere terminado"
+else
+    echo "problemas para terminar Premiere, revisar manualmente"
+fi
+
+sleep 2
+
+echo "bot termino su ejecución sin errores, terminar consola por favor"
